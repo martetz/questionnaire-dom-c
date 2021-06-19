2e128852734f538226ee5b9ea963c94706a9b7fa
Marta Khromova <71217056+martetz@users.noreply.github.com>
2021-06-20 01:20:27 +0300

    fixed the CORS-problem on the "server" side

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import {Component} from 'react';
+import Card from './components/Card';
+
+const url = 'http://localhost:3001/api/quations';
+
 
 export default class App extends Component{
-    render(){
-        return (
-            <div className='container'></div>
+    constructor(props){
+        super(props);
+        this.state = {
+            data: false
+        }
+    }
+    async componentDidMount(){
+        const res = await fetch(url);
+        const data = await res.json();
+        this.setState({
+            data: data,
+        })        
+    }
+
+    initAllQuations(q){
+       const cardsList = q.map(card => (<Card key={'key-'+ card.id} text={card.quation}/>))
+           return (
+            <>
+                {cardsList}
+            </>
         )
+    }
+
+    render(){
+        if(!this.state.data){
+            return (<div></div>)
+        } else {
+            let q = this.state.data;
+            return (
+                <div className='container'>
+                    <h1>Что вы знаете о ДомКлик?</h1>
+                    {this.initAllQuations(q)}
+                </div>
+            )
+        }
+     
     }
 }
